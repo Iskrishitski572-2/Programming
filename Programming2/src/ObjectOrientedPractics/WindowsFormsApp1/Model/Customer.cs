b1ec6b39e169ec32ae2448f619eef3d7f6ab91df
Iskrishitski572-2 <dimaiskr21@gmail.com>
2023-10-25 17:48:42 +0700

    model done

---
+++ Programming2/src/ObjectOrientedPractics/WindowsFormsApp1/Model/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Model
+{
+    public class Customer
+    {
+        private string _fullName;
+
+        private string _adress;
+
+        public int Id { get; }
+
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                //VALUDATOR
+                _fullName = value;
+            }
+        }
+
+        public string Adress
+        {
+            get => _adress;
+            set
+            {
+                //VALUDATOR
+                _adress = value;
+            }
+        }
+
+        public Customer(string fullName, string adress, int id)
+        {
+            FullName = fullName;
+            Adress = adress;
+
+            //Id = GENERATOR id;
+
+
+        }
+
+
+    }
+}
--- Programming2/src/ObjectOrientedPractics/WindowsFormsApp1/Model/Item.cs
+++ Programming2/src/ObjectOrientedPractics/WindowsFormsApp1/Model/Item.cs
@@ -15,5 +15,44 @@ namespace ObjectOrientedPractics.Model
         private double _cost;
 
         public int Id { get; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                //VALIDATOR
+                _name = value;
+            }
+        }
+
+        public string Info
+        {
+            get => _info;
+            set
+            {
+                //VALIDATOR
+                _info = value;
+            }
+        }
+
+        public double Cost
+        {
+            get => _cost;
+            set
+            {
+                //VALUDATOR
+                _cost = value;
+            }
+        }
+
+        public Item(string name, string info, double cost)
+        {
+            Name = name;
+            Info = info;
+            Cost = cost;
+            //Id = ID GENERATOR id;
+        }
     }
+
 }
