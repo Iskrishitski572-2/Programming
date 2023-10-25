530de28271ae8773fce90771cdb176f1c2d21427
Iskrishitski572-2 <dimaiskr21@gmail.com>
2023-10-25 17:29:48 +0700

    OOP lab1

---
+++ Programming2/src/ObjectOrientedPractics/WindowsFormsApp1/Model/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class Item
+    {
+        private string _name;
+
+        private string _info;
+
+        private double _cost;
+
+        public int Id { get; }
+    }
+}
